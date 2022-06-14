f02ecbca8c33827d797cdd1d55f9eaeb7daefca1
Nadia Shovkovy <nadiashovkovy@gmail.com>
2022-06-14 12:02:40 -0700

    Initial Commit

---
+++ My Steps Tracker./My_Steps_Tracker_App.swift
@@ -0,0 +1,17 @@
+//
+//  My_Steps_Tracker_App.swift
+//  My Steps Tracker.
+//
+//  Created by N. Shovkovy on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct My_Steps_Tracker_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
